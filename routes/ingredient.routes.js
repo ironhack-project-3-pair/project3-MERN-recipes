01c59f72cfeec21bbe1e915bcd24376006887a05
linhvnde <linh.nh.tyty@gmail.com>
2023-06-28 20:18:47 +0200

    added routes to get ingredients list and details

--- routes/ingredient.routes.js
+++ routes/ingredient.routes.js
@@ -1,28 +1,61 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const Recipe = require("../models/Recipe.model");
-const Ingredient = require("../models/Ingredient.model");
+const Recipe = require('../models/Recipe.model');
+const Ingredient = require('../models/Ingredient.model');
 
 // POST /api/ingredients
-router.post("/ingredients", (req, res, next) => {
-    const { name } = req.body;
-    Ingredient.create({ name })
-        .then(response => {
-            res.status(201).json({message: "ingredient document created: " + response.name})
-        })
-        .catch(e => console.log("Error creating ingredient: " + e))
+router.post('/ingredients', (req, res, next) => {
+  const { name } = req.body;
+  Ingredient.create({ name })
+    .then((response) => {
+      res
+        .status(201)
+        .json({ message: 'ingredient document created: ' + response.name });
+    })
+    .catch((err) => {
+      console.log('error creating ingredient', err);
+      res.status(500).json({
+        message: 'error creating ingredient',
+        error: err,
+      });
+    });
 });
 
 // GET /api/ingredients
 router.get('/ingredients', (req, res, next) => {
-    
+  Ingredient.find()
+    .then((response) => {
+      res.json(response);
+    })
+    .catch((err) => {
+      console.log('error getting list of ingredients', err);
+      res.status(500).json({
+        message: 'error getting list of ingredients',
+        error: err,
+      });
+    });
 });
 
 // GET /api/ingredients/:ingredientId
 router.get('/ingredients/:ingredientId', (req, res, next) => {
+    const { ingredientId } = req.params;
 
+    if (!mongoose.Types.ObjectId.isValid(ingredientId)) {
+      res.status(400).json({ message: 'Specified id is not valid' });
+      return;
+    }
+
+    Ingredient.findById(ingredientId)
+      .then((response) => res.json(response))
+      .catch((err) => {
+        console.log('error getting details of a ingredient', err);
+        res.status(500).json({
+          message: 'error getting details of a ingredient',
+          error: err,
+        });
+      });
 });
 
 module.exports = router;
