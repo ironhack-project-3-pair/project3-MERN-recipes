667fc1ace1388ef822b0335dc8903c1adda0c83a
hymced <hymced@gmail.com>
2023-07-03 11:10:57 +0200

    feat: userIngredients model + routes

--- models/User.model.js
+++ models/User.model.js
@@ -1,5 +1,7 @@
 const { Schema, model } = require("mongoose");
 
+const mongoose = require("mongoose");
+
 const userSchema = new Schema(
   {
     email: {
@@ -16,6 +18,63 @@ const userSchema = new Schema(
     name: {
       type: String,
       required: [true, "Name is required."],
+    },
+
+    userIngredients: {
+      type: [
+        {
+            ingredient: {
+                type: Schema.Types.ObjectId, 
+                ref: 'Ingredient',
+                required: true,
+                // unique: true // does not work to prevent duplicate ingredients, it enforces uniqueness across different documents only
+            }, 
+            qtyInGrams: {
+                type: Number,
+                required: true
+            }
+        },
+      ],
+      // default: undefined // results in userIngredients: null
+      // default: [] // results in userIngredients: undefined
+      validate: {
+        // return false or throwing an error means validation failed
+        validator: function(userIngredients) {
+          // if (this instanceof mongoose.Document) { // document validator
+          //   console.log("mongoose.Document");
+          //   console.log(this.userIngredients);
+          // } else if (this instanceof mongoose.Query) { // update validator
+          //   console.log("mongoose.Query");
+          //   console.log(this.get('userIngredients'));
+          //   // $push, $addToSet, $pull, and $pullAll validation does not run any validation on the array itself, 
+          //   // only individual elements of the array
+          //   // so in this case, userIngredients contains only the updated elements, not the updated array
+          //   // https://mongoosejs.com/docs/validation.html#update-validators
+          //   // https://mongoosejs.com/docs/validation.html#update-validators-and-this
+          //   // https://mongoosejs.com/docs/validation.html#update-validators-only-run-on-updated-paths
+          //   // https://mongoosejs.com/docs/validation.html#update-validators-only-run-for-some-operations
+          // } else { // bug??? happens with FindOneAndUpdate + $push
+          //   console.log("validator else")
+          //   // console.log(this)
+          //   // <ref *1> Object [global] {
+          //   //   global: [Circular *1],
+          //   // this is NodeJS global object with a circular reference (node repl)
+          //   // console.log(JSON.stringify(this))
+          //   // CONCLUSION: not possible to access the updated array with FindOneAndUpdate + $push to check for duplicates in the updated array
+          // }
+          for (let i = 0; i < userIngredients.length; i++) {
+            const duplicates = userIngredients.filter(userIngredient => {
+              return userIngredient.ingredient._id.toString() === userIngredients[i].ingredient._id.toString()
+              // .toString() important!
+          })
+          if (duplicates.length > 1) {
+            return false;
+            }
+          }
+          return true;
+        },
+        message: 'duplicate ingredient'
+      }
     },
   },
   {
---
+++ routes/userIngredient.routes.js
@@ -0,0 +1,245 @@
+const router = require('express').Router();
+
+const mongoose = require('mongoose');
+
+const User = require('../models/User.model');
+
+// GET /api/userIngredients
+router.get('/userIngredients', (req, res, next) => {
+  let { userId } = req.body; // may be used by admin
+  if (!userId) userId = req.payload._id // fallback to current user
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    res.status(404).json({ message: 'User id is not valid' });
+    return;
+  }
+
+  User.findOne({ _id: userId }, { userIngredients: 1 }) // using a projection, returns an object containing only the array and its id)
+    .populate("userIngredients.ingredient")
+    .then(response => {
+      res.status(201).json(response.userIngredients)
+    })
+    .catch(err => {
+      console.log("error getting userIngredients", err);
+      res.status(500).json({
+        message: "error getting userIngredients",
+        error: err
+      });
+    })
+});
+
+// GET /api/userIngredients/id
+router.get('/userIngredients/:userIngredientId', (req, res, next) => {
+  let { userId } = req.body; // may be used by admin
+  if (!userId) userId = req.payload._id // fallback to current user
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    res.status(404).json({ message: 'User id is not valid' });
+    return;
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(req.params.userIngredientId)) {
+    res.status(404).json({ message: 'userIngredient id is not valid' });
+    return;
+  }
+
+  const { userIngredientId } = req.params;
+  User.findOne({ _id: userId }, { userIngredients: { $elemMatch: { _id: userIngredientId } } }) // using a projection, returns an object containing only the array (with a single element) and its id)
+    .populate("userIngredients.ingredient")
+    .then(response => {
+      res.status(201).json(response.userIngredients[0])
+    })
+    .catch(err => {
+      console.log("error getting userIngredient", err);
+      res.status(500).json({
+        message: "error getting userIngredient",
+        error: err
+    });
+    })
+});
+
+// POST /api/userIngredients
+router.post('/userIngredients', (req, res, next) => {
+  let { userId } = req.body; // may be used by admin
+  if (!userId) userId = req.payload._id // fallback to current user
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    res.status(404).json({ message: 'User id is not valid' });
+    return;
+  }
+
+  const newUserIngredient = {
+    ingredient: req.body.ingredientId,
+    qtyInGrams: req.body.qtyInGrams,
+  };
+
+  // User.findByIdAndUpdate(userId, 
+  //   // { $push: { userIngredients: newUserIngredient } }, // won't be validated in schema to prevent duplicates
+  //   // { $addToSet: { userIngredients: newUserIngredient } }, // adds a value to an array unless the value is already present, but does not work in this case because not the same value (object) even if same properties
+  //   { new: true })
+  //   .populate("userIngredients.ingredient")
+
+  User.findOne({ _id: userId }, { userIngredients: { $elemMatch: { ingredient: { _id: newUserIngredient.ingredient } } } }) // with $elemMatch operator in projection
+    .then(response => {
+      if (response.userIngredients.length === 0) {
+        // throw new Error("Ingredient not found");
+        // create the userIngredients
+        return User.findByIdAndUpdate(userId, 
+          { $push: { userIngredients: newUserIngredient } }, // won't be validated in schema to prevent duplicates
+          { new: true })
+          .populate("userIngredients.ingredient")
+          .then(response => {
+            res.status(201).json(response) // CREATED
+          })
+      } else {
+        // // increment the qty
+        // // scenario moved to PUT request
+        // return User.findByIdAndUpdate(userId, 
+        //   { $inc: { "userIngredients.$[elem].qtyInGrams": newUserIngredient.qtyInGrams } }, 
+        //   { 
+        //     new: true, 
+        //     arrayFilters: [{ "elem.ingredient": newUserIngredient.ingredient }] 
+        //   })
+        //   .populate("userIngredients.ingredient")
+        //   .then(response => {
+        //     res.status(200).json(response) // OK
+        //   })
+        res.status(409).json({ // CONFLICT
+          message: "conflict creating a new userIngredient: item already exist, to update use PUT verb instead (same endpoint + /userIngredientId)",
+          userIngredientId: response.userIngredients[0]._id
+        })
+      }
+    })
+    .catch(err => {
+      console.log("error creating a new userIngredient", err);
+      res.status(500).json({
+        message: "error creating a new userIngredient",
+        error: err
+      });
+    })
+});
+
+// PUT /api/userIngredients/id
+router.put('/userIngredients/:userIngredientId', (req, res, next) => {
+  let { userId } = req.body; // may be used by admin
+  if (!userId) userId = req.payload._id // fallback to current user
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    res.status(404).json({ message: 'User id is not valid' });
+    return;
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(req.params.userIngredientId)) {
+    res.status(404).json({ message: 'userIngredient id is not valid' });
+    return;
+  }
+
+  const newUserIngredient = {
+    ingredient: req.body.ingredientId,
+    qtyInGrams: req.body.qtyInGrams,
+  };
+
+  const { userIngredientId } = req.params;
+  User.findOneAndUpdate(
+    { _id: userId, 'userIngredients._id': userIngredientId },
+    { $inc: { 'userIngredients.$.qtyInGrams': newUserIngredient.qtyInGrams } }, // using positional operator
+    { new: true }
+  )
+    .populate("userIngredients.ingredient")
+    .then(response => {
+        res.status(200).json(response)
+      })
+    .catch(err => {
+      console.log("error updating userIngredient", err);
+      res.status(500).json({
+        message: "error updating userIngredient",
+        error: err
+      });
+    })
+});
+
+// PUT /api/userIngredients
+router.put('/userIngredients', (req, res, next) => {
+  let { userId } = req.body; // may be used by admin
+  if (!userId) userId = req.payload._id // fallback to current user
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    res.status(404).json({ message: 'User id is not valid' });
+    return;
+  }
+
+  const newUserIngredients = req.body.userIngredients;
+
+  User.findOneAndUpdate(
+    { _id: userId },
+    { userIngredients: newUserIngredients },
+    { new: true, runValidators: true }
+  )
+    .populate("userIngredients.ingredient")
+    .then(response => {
+        res.status(200).json(response)
+      })
+    .catch(err => {
+      console.log("error updating userIngredients", err);
+      res.status(500).json({
+        message: "error updating userIngredients",
+        error: err
+      });
+    })
+});
+
+// DELETE /api/userIngredients/id
+router.delete('/userIngredients/:userIngredientId', (req, res, next) => {
+  let { userId } = req.body; // may be used by admin
+  if (!userId) userId = req.payload._id // fallback to current user (routes mounted and protected)
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    res.status(404).json({ message: 'User id is not valid' });
+    return;
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(req.params.userIngredientId)) {
+    res.status(404).json({ message: 'userIngredient id is not valid' });
+    return;
+  }
+
+  const { userIngredientId } = req.params;
+  User.findByIdAndUpdate(userId, { $pull: { userIngredients: { _id: userIngredientId } } }, { new: true })
+  // User.findByIdAndUpdate(userId, { $pull: { userIngredients: { _id: { $in: [userIngredientId] } } } }, { new: true }) // ok
+  
+  // User.findByIdAndUpdate(userId, { $pullAll: { userIngredients: [{ _id: userIngredientId }] } }, { new: true })
+  // // mentionned here but it doesn't work and it is not indented to work this way
+  // // https://stackoverflow.com/questions/5890898/confused-as-to-how-pullall-works-in-mongodb/5894260#5894260
+  // // $pullAll works only if values are exact matches and condition operators not allowed, it takes an array, not a document (via a query)
+  // User.findOne({ _id: userId }, { userIngredients: { $elemMatch: { _id: userIngredientId } } })
+  //   .then(response => {
+  //     const userIngredient = response.userIngredients[0];
+  //     return User.findByIdAndUpdate(userId, { $pullAll: { userIngredients: [userIngredient] } }, { new: true })
+  //       .populate("userIngredients.ingredient")
+  //   })
+  //   .then(response => res.status(200).json(response))
+  //   .catch(err => {
+  //     console.log("error deleting userIngredient", err);
+  //     res.status(500).json({
+  //         message: "error deleting userIngredient",
+  //         error: err
+  //     });
+  //   }) // ok but not efficient codewise...
+});
+
+// DELETE /api/userIngredients/
+router.delete('/userIngredients', (req, res, next) => {
+  let { userId } = req.body; // may be used by admin
+  if (!userId) userId = req.payload._id // fallback to current user (routes mounted and protected)
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    res.status(404).json({ message: 'User id is not valid' });
+    return;
+  }
+
+  User.findByIdAndUpdate(userId, { $unset: { userIngredients: "" } }, { new: true }) // completely remove the field
+  // User.findByIdAndUpdate(userId, { $set: { userIngredients: [] } }, { new: true }) 
+    .then(response => res.status(200).json(response))
+    .catch(err => {
+      console.log("error deleting userIngredients", err);
+      res.status(500).json({
+          message: "error deleting userIngredients",
+          error: err
+      });
+    })
+});
+
+module.exports = router;
