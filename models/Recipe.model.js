d5f17745fbddb48baa2c51a693540a81df16d661
linhvnde <linh.nh.tyty@gmail.com>
2023-07-05 15:18:31 +0200

    Feat: Validator for Recipe.model

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,32 +2,51 @@ const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
 const recipeSchema = new Schema({
-    name: {
-        type: String,
-        required: [true, "Please provide a valid name"],
-        unique: true
+  name: {
+    type: String,
+    required: [true, 'Name is required. Please provide a valid name'],
+    unique: true,
+  },
+  instructions: String,
+  durationInMin: Number,
+  recipeIngredients: {
+    type: [
+      {
+        ingredient: {
+          type: Schema.Types.ObjectId,
+          ref: 'Ingredient',
+          required: true,
+          validate: {
+            validator: function (ingredientInputValue) {
+              return ingredientInputValue != null && ingredientInputValue != '';
+            },
+            message:
+              "Ingredient's name is required. Please provide valid ingredient",
+          },
         },
-    instructions: String,
-    durationInMin: Number,
-    recipeIngredients: {
-        type: [
-            {
-                ingredient: {
-                    type: Schema.Types.ObjectId, 
-                    ref: 'Ingredient',
-                    required: true
-                }, 
-                qtyInGrams: {
-                    type: Number,
-                    required: true,
-                    min: 0
-                }
-            }
-        ]
-    },
-    picture: {
-        type: String
-    }
+        qtyInGrams: {
+          type: Number,
+          required: true,
+          min: 1,
+          validate: {
+            validator: function (ingredientQtyInputValue) {
+              return ingredientQtyInputValue > 0;
+            },
+            message: 'Quantity is required. Please provide valid quantity',
+          },
+        },
+      },
+    ],
+    validate: [ingredientsLimit, '{PATH} needs to have at least 1 ingredient'],
+  },
+  picture: {
+    type: String,
+  },
 });
+
+// Validate number of ingredients of created Recipe
+function ingredientsLimit(recipeIngredients) {
+  return recipeIngredients.length > 0;
+}
 
 module.exports = model('Recipe', recipeSchema);
